78fbdbadfe36b50c1bc2e26c66eca3aef37f8938
MahmoudKamal01 <53492856+MahmoudKamal01@users.noreply.github.com>
2025-06-19 06:00:42 +0300

    modify api

--- frontend/src/configs/api.js
+++ frontend/src/configs/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL
+  baseURL: '/api'
 });
 
 // Check if there's a token in the cookies and add it to the headers if it exists
